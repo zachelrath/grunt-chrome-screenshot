f37c591e5f9f6ce2ed35f14a86161a9092309d2e
Zach McElrath <zach@skuidify.com>
2018-07-23 16:25:05 -0400

    v0.2.0

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
 # grunt-chrome-screenshot
+
 Grunt plugin for taking screenshots using headless chrome / Puppeteer
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-chrome-screenshot --save-dev
+```
+
+Once the plugin has been installed, you can load it inside your Gruntfile using
+
+```js
+grunt.loadNpmTasks('grunt-chrome-screenshot');
+```
+
+## The "chrome_screenshot" task
+
+### Overview
+In your project's Gruntfile, add a section named `chrome_screenshot` to the data object passed into `grunt.initConfig()`. Alternatively, you can use `grunt.config.set(...)` to add a configuration within your Gruntfile or Grunt task definitions.
+
+```js
+grunt.initConfig({
+  chrome_screenshot: {
+    desktop_shots: {
+      options: {
+        viewport: {
+          isLandscape: true,
+          width: 1024,
+          height: 768,
+        },
+        targets: [
+          {
+              src: "http://localhost:4321/fixtures/main-theme.html",
+              dest: "screenshots/desktop/main.png"
+          },
+          {
+              src: "http://localhost:4321/fixtures/other-theme.html",
+              dest: "screenshots/desktop/other.png",
+          },
+        ],
+      },
+    },
+    mobile_shots: {
+      options: {
+        viewport: {
+          width: 1024,
+          height: 768,
+          isLandscape: false,
+          isMobile: true,
+        },
+        targets: [
+          {
+              src: "http://localhost:4321/fixtures/mobile-main-theme.html",
+              dest: "screenshots/mobile/main.png",
+          },
+          {
+              src: "http://localhost:4321/fixtures/mobile-new-theme.html",
+              dest: "screenshots/mobile/new.png",
+          },
+        ],
+      },
+    },
+  },
+});
+```
+
+This plugin uses [Puppeteer](https://github.com/GoogleChrome/puppeteer) to instrument a headless Chromium instance. To use headless Chromium in CI, you will need to follow the environment-specific [troubleshooting steps](https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md) in Puppeteer's documentation.
+
+There are a few configuration parameters that you can use to configure how Chromium is invoked by Puppeteer and where a preloaded binary for Chromium is located on the host OS.
+
+To run in Alpine, for example, you will very likely need to use the following configuration options:
+
+```js
+grunt.initConfig({
+  chrome_screenshot: {
+    alpine_ci_example: {
+      options: {
+        // Specify CLI arguments that Puppeteer should use when invoking Chromium
+        chromeArgs: [
+          "--disable-dev-shm-usage",
+        ],
+        // Specify where a preloaded Chromium binary is located
+        chromeExecutable: "/usr/bin/chromium-browser",
+        // Environment variables for Puppeteer to use when invoking Chromium
+        env: {
+          TZ: "UTC"
+        },
+        ...
+        targets: [ ... ],
+      },
+    },
+  },
+});
+```
+
+## Contributing
+ESLint is used to enforce code style - please run `npm run lint` to ensure that your changes conform to the styleguide defined by the .eslintrc.json.
+
+There are no unit tests yet (soon, hopefully!).
--- tasks/chrome-screenshot.js
+++ tasks/chrome-screenshot.js
@@ -5,93 +5,94 @@ const puppeteer = require("puppeteer");
 
 module.exports = function(grunt) {
 
-	async function init(options, done) {
+  async function init(options, done) {
 
-		const browser = await puppeteer.launch({
-			args: options.chromeArgs || [],
-			executablePath: options.chromeExecutable,
-			headless: true,
-			ignoreHTTPSErrors: true,
-		});
-		const targets = options.targets;
+    const browser = await puppeteer.launch({
+      args: options.chromeArgs || [],
+      executablePath: options.chromeExecutable,
+      env: options.env,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    });
+    const targets = options.targets;
 
-		try {
-			await Promise.all(targets.map(({
-				dest,
-				src,
-			}) => {
-				return screenshotFile(src, dest, browser, options);
-			}));
-		} catch (err) {
-			grunt.log.error("Failed to capture screenshots of all files: ", err);
-			done(false);
-		}
+    try {
+      await Promise.all(targets.map(({
+        dest,
+        src,
+      }) => {
+        return screenshotFile(src, dest, browser, options);
+      }));
+    } catch (err) {
+      grunt.log.error("Failed to capture screenshots of all files: ", err);
+      done(false);
+    }
 
-		await browser.close();
-		done();
-	}
+    await browser.close();
+    done();
+  }
 
-	async function screenshotFile(srcUrl, path, browser, options) {
-		const page = await browser.newPage();
-		const {
-			type,
-		} = options;
+  async function screenshotFile(srcUrl, path, browser, options) {
+    const page = await browser.newPage();
+    const {
+      type,
+    } = options;
 
-		const screenshotOptions = {
-			fullPage: options.fullPage,
-			omitBackground: options.omitBackground,
-			path,
-			type,
-		};
+    const screenshotOptions = {
+      fullPage: options.fullPage,
+      omitBackground: options.omitBackground,
+      path,
+      type,
+    };
 
-		// Quality is only supported for jpeg
-		if (type === "jpeg") {
-			screenshotOptions.quality = options.quality;
-		}
+    // Quality is only supported for jpeg
+    if (type === "jpeg") {
+      screenshotOptions.quality = options.quality;
+    }
 
-		await page.setViewport(options.viewport);
+    await page.setViewport(options.viewport);
 
-		grunt.verbose.writeln(`Capturing ${type} screenshot of ${srcUrl}`);
+    grunt.verbose.writeln(`Capturing ${type} screenshot of ${srcUrl}`);
 
-		try {
-			await page.goto(srcUrl, {
-				timeout: options.timeout,
-				waitUntil: options.waitUntil,
-			});
-			await page.screenshot(screenshotOptions);
-			grunt.verbose.writeln(`Wrote ${type} screenshot to ${path}`);
-		} catch (err) {
-			grunt.log.error(`Failed to capture screenshot at ${srcUrl}: ${err.message}`);
-			return Promise.reject(`Failed to capture screenshot at ${srcUrl}: ${err.message}`);
-		}
-	}
+    try {
+      await page.goto(srcUrl, {
+        timeout: options.timeout,
+        waitUntil: options.waitUntil,
+      });
+      await page.screenshot(screenshotOptions);
+      grunt.verbose.writeln(`Wrote ${type} screenshot to ${path}`);
+    } catch (err) {
+      grunt.log.error(`Failed to capture screenshot at ${srcUrl}: ${err.message}`);
+      return Promise.reject(`Failed to capture screenshot at ${srcUrl}: ${err.message}`);
+    }
+  }
 
-	grunt.registerMultiTask("chrome_screenshot", "QUnit task for taking screenshots with headless Chrome", function() {
+  grunt.registerMultiTask("chrome_screenshot", "QUnit task for taking screenshots with headless Chrome", function() {
 
-		const done = this.async();
+    const done = this.async();
 
-		const opts = this.options({
-			fullPage: false,
-			omitBackground: false,
-			quality: 100,
-			type: "png", // jpeg, png
-			viewport: {
-				width: 800,
-				height: 600,
-				deviceScaleFactor: 1,
-				hasTouch: false,
-				isLandscape: true,
-				isMobile: false,
-			},
-			waitUntil: "networkidle0",
-		});
+    const opts = this.options({
+      fullPage: false,
+      omitBackground: false,
+      quality: 100,
+      type: "png", // jpeg, png
+      viewport: {
+        width: 800,
+        height: 600,
+        deviceScaleFactor: 1,
+        hasTouch: false,
+        isLandscape: true,
+        isMobile: false,
+      },
+      waitUntil: "networkidle0",
+    });
 
-		if (!opts.targets.length) {
-			grunt.fail.fatal("You must provide at least one screenshot target.");
-			return;
-		}
+    if (!opts.targets.length) {
+      grunt.fail.fatal("You must provide at least one screenshot target.");
+      return;
+    }
 
-		init(opts, done);
+    init(opts, done);
 
-	});
+  });
 };
